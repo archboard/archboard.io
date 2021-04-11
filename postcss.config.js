c894cef26f5c01d02d3f6c59a591cc9bec8975ee
Grant Holle <hollegrant@gmail.com>
2021-04-11 09:27:44 +0800

    Add footer subscription form

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies, global-require */
 const plugins = [
   require('postcss-import'),
-  require('@tailwindcss/jit'),
+  require('tailwindcss'),
   require('autoprefixer'),
 ];
 
